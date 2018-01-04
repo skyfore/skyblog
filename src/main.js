89539c5d893f6f6f17067abbbe0755de6efd4a0c
skyfore <skyfore@live.com>
2018-01-04 16:39:27 +0800

    feat(Add material-css): Add material-css to support material
    *
    
    BREAKING CHANGE: Material Design !

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+//  vue-material css global import
+import 'vue-material/dist/vue-material.min.css'
+
+//  import whole vue-material but will split soon
+import VueMaterial from 'vue-material'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
